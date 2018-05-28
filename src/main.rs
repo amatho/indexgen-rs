acf385f77527733518df2471fdb818a3ef50aa4f
Amandus Thorsrud <ama.thorsrud@gmail.com>
2018-05-28 12:23:28 +0200

    Small refactoring improvements
    Change some argument and return types to be better suited

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod template;
 
 use std::fs;
-use std::path;
+use std::path::{Path, PathBuf};
+use std::ffi::OsString;
 use std::env;
 
 struct FileData {
@@ -22,7 +23,7 @@ fn main() {
   let paths = match fs::read_dir(&working_dir) {
     Ok(p) => p,
     Err(_) => {
-      println!("Could not read directory {}.", &working_dir);
+      println!("Could not read directory {}.", str_expect(&working_dir));
       return;
     }
   };
@@ -30,8 +31,8 @@ fn main() {
   println!("Current Directory: {:?}", current_dir);
   let mut file_vec: Vec<FileData> = vec![];
 
-  for path in paths {
-    let path = path.unwrap().path();
+  for entry in paths {
+    let path = entry.unwrap().path();
 
     if should_ignore(&path) {
       continue;
@@ -45,35 +46,34 @@ fn main() {
   write_index(data, working_dir);
 }
 
-fn get_wd(args: &Vec<String>) -> String {
+fn get_wd(args: &Vec<String>) -> PathBuf {
   let wd = if args.len() > 1 {
     &args[1]
   } else {
     "."
   };
 
-  String::from(wd)
+  PathBuf::from(wd)
 }
 
-fn get_current_dir(working_dir: &String) -> String {
-  let wd = working_dir.as_str();
-  let path_buf = match wd {
+fn get_current_dir(working_dir: &Path) -> String {
+  let path_buf = match str_expect(working_dir) {
     "." => {
       env::current_dir().unwrap()
     },
-    _ => path::Path::new(working_dir).to_path_buf()
+    _ => Path::new(working_dir).to_path_buf()
   };
 
-  path_buf.file_name().unwrap().to_str().unwrap().to_string()
+  get_file_name(&path_buf)
 }
 
-fn build_file_data(path: path::PathBuf) -> FileData {
+fn build_file_data(path: PathBuf) -> FileData {
   let file_type = match path.is_dir() {
     true => String::from("dir"),
     false => String::from("file")
   };
 
-  let file_name = String::from(path.file_name().unwrap().to_str().unwrap());
+  let file_name = get_file_name(&path);
 
   FileData {
     file_type,
@@ -81,15 +81,24 @@ fn build_file_data(path: path::PathBuf) -> FileData {
   }
 }
 
-fn should_ignore(path: &path::PathBuf) -> bool {
-  let file_name = String::from(path.file_name().unwrap().to_str().unwrap());
+fn should_ignore(path: &PathBuf) -> bool {
+  let file_name = get_file_name(path);
   let first_char = &file_name[0..1];
 
   first_char == "." || &file_name[..] == "index.html"
 }
 
-fn write_index(data: Data, working_dir: String) {
+fn write_index(data: Data, working_dir: PathBuf) {
   let contents = template::generate(data);
-  let write_path = path::Path::new(&working_dir).join("index.html");
+  let write_path = Path::new(&working_dir).join("index.html");
   fs::write(write_path, contents).expect("Could not write index.html.");
+}
+
+fn get_file_name(path_buf: &PathBuf) -> String {
+  OsString::from(path_buf.file_name().expect("Could not find filename component of path."))
+    .into_string().expect("Path does not contain valid UTF-8.")
+}
+
+fn str_expect(p: &Path) -> &str {
+  p.to_str().expect("Not valid unicode.")
 }
